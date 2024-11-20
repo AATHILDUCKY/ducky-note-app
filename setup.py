5a4ca1360ccc23f9ec2cebaf3b40d7d6c1942eb4
unknown <adhiyazainab2004@gmail.com>
2024-11-20 16:46:42 +0530

    modify setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="note-taking-app",
+    name="ducky-notes",
     version="1.0.0",
     description="A simple note-taking application with a Tkinter GUI.",
-    author="Your Name",
-    author_email="your.email@example.com",
-    url="https://github.com/yourusername/note-taking-app",  # Your GitHub repo or project URL
+    author="Aathil Ducky",
+    author_email="aathilducky@gmail.com",
+    url="https://github.com/AATHILDUCKY/ducky-note-app",  # Your GitHub repo or project URL
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
